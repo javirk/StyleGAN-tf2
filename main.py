bb203659a43eda61c3c073f3188309a3fab963c0
javier <jgamtejero@gmail.com>
2020-02-16 16:59:18 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from utils.model import StyleGAN
 import numpy as np
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
 
 MAPPING_SIZE = 8
 NOISE_SHAPE = [256, 256, 1]
